69c7b09b577d7721161d2f55b723dfe771d1d6b6
jaypinedabengco <jaybengcodeveloper@gmail.com>
2018-09-12 17:02:39 +0800

    INIT

--- App.vue
+++ App.vue
@@ -1,17 +1,26 @@
 <template>
   <view class="container">
-    <text class="text-color-primary">My Vue Native Apps</text>
+    <view :style="{flex: 1, justifyContent: 'center'}">
+      <activity-indicator size="large" color="#0000ff" />
     </view>
+  </view>
 </template>
- 
+
+<script>
+export default {
+  methods: {
+    renderList: function(item) {
+      return `<Text>{${item}}<Text>`;
+    }
+  }
+};
+</script>
+
 <style>
 .container {
   background-color: white;
   align-items: center;
   justify-content: center;
   flex: 1;
-}
-.text-color-primary {
-  color: blue;
 }
 </style>
